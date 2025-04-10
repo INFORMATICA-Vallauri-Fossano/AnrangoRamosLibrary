f28b2840ed516240bec9103b378e9c2503efa15f
Gionsi Anrrango Ramos <Acharig47@gmail.com>
2025-04-10 13:28:06 +0200

    targa validazine regex

--- AnrangoRamosLibrary/RegexUtilities.cs
+++ AnrangoRamosLibrary/RegexUtilities.cs
@@ -7,8 +7,14 @@ using System.Text.RegularExpressions;
 
 namespace AnrangoRamosLibrary
 {
-    class RegexUtilities
+    public class RegexUtilities
     {
+        public static bool IsValidTarga(string targa)
+        {
+            bool ok=false;
+            if(Regex.IsMatch(targa,@"^[A-Z]{2}\d{3}[A-Z]{2}$")) ok=true;
+            return ok;
+        }
         public static bool IsSafePassword(string password)
         {
             if (string.IsNullOrEmpty(password))
